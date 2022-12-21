caf3d2b097f3bcb22e8eeb0a5ef33e3327b4d3d2
Innocent <paulinnocent04@gmail.com>
2022-12-21 22:41:41 +0000

    updated

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -25,56 +25,56 @@ const Home = () => {
                     <div style={{ borderTop: 'none' }} className="bg-[rgba(0,0,0,.5)] rounded-b-[10px] py-[20px] px-[20px] border-[5px] border-[#EA9E1A]">
                         <div className="flex gap-5">
                             <div className="flex-1 flex flex-col items-center justify-center border-[3px] pt-2 pb-4 border-[#EA9E1A] rounded-[10px]">
-                                <p className="text-[17px] text-white font-akkuratPro">Total Pool</p>
-                                <h2 className="text-[28.8px] text-[#F08717] font-akkuratPro">0.00 BNB</h2>
+                                <p className="text-[14px] md:text-[17px] text-white font-akkuratPro">Total Pool</p>
+                                <h2 className="text-[17px] md:text-[28.8px] text-[#F08717] font-akkuratPro">0.00 BNB</h2>
                             </div>
                             <div className="flex-1 flex flex-col items-center justify-center border-[3px] pt-2 pb-4 border-[#EA9E1A] rounded-[10px]">
-                                <p className="text-[17px] text-white font-akkuratPro">Ticket Remaining</p>
-                                <h2 className="text-[28.8px] text-[#F08717] font-akkuratPro">1000000</h2>
+                                <p className="text-[14px] md:text-[17px] text-white font-akkuratPro">Ticket Remaining</p>
+                                <h2 className="text-[17px] md:text-[28.8px] text-[#F08717] font-akkuratPro">1000000</h2>
                             </div>
                         </div>
 
-                        <p className="text-lg text-center text-white py-3 font-akkuratPro">Ticket Sales have now CLOSED for this draw</p>
+                        <p className="text-[14px] md:text-lg text-center text-white py-3 font-akkuratPro">Ticket Sales have now CLOSED for this draw</p>
 
                         <div className="flex justify-between font-anton">
                             <div className="text-center">
-                                <div style={{ background: "linear-gradient(165deg, rgba(132, 93, 13, 1) 0%, rgba(162, 168, 68, 1) 56%)" }} className="px-[20px] py-[5px] rounded-[10px] leading-[109.02px] text-[40px] md:text-[72.42px] text-[#330707] font-bold">00</div>
-                                <p className="mt-1 text-lg text-white font-akkuratPro">Hours</p>
+                                <div style={{ background: "linear-gradient(165deg, rgba(132, 93, 13, 1) 0%, rgba(162, 168, 68, 1) 56%)" }} className="px-[16px] md:px-[20px] py-0 md:py-[5px] rounded-[10px] leading-[109.02px] text-[30px] md:text-[72.42px] text-[#330707] font-bold">00</div>
+                                <p className="mt-1 text-[14px] md:text-lg text-white font-akkuratPro">Hours</p>
                             </div>
                             <div className="flex flex-col justify-center gap-5 -translate-y-2 px-5">
                                 <div style={{ background: "linear-gradient(165deg, rgba(132, 93, 13, 1) 0%, rgba(162, 168, 68, 1) 56%)" }} className="w-[15px] h-[15px] rounded-full"></div>
                                 <div style={{ background: "linear-gradient(165deg, rgba(132, 93, 13, 1) 0%, rgba(162, 168, 68, 1) 56%)" }} className="w-[15px] h-[15px] rounded-full"></div>
                             </div>
                             <div className="text-center">
-                                <div style={{ background: "linear-gradient(165deg, rgba(132, 93, 13, 1) 0%, rgba(162, 168, 68, 1) 56%)" }} className="px-[20px] py-[5px] rounded-[10px] leading-[109.02px] text-[40px] md:text-[72.42px] text-[#330707] font-bold">00</div>
-                                <p className="mt-1 text-lg text-white font-akkuratPro">Minutes</p>
+                                <div style={{ background: "linear-gradient(165deg, rgba(132, 93, 13, 1) 0%, rgba(162, 168, 68, 1) 56%)" }} className="px-[16px] md:px-[20px] py-0 md:py-[5px] rounded-[10px] leading-[109.02px] text-[30px] md:text-[72.42px] text-[#330707] font-bold">00</div>
+                                <p className="mt-1 text-[14px] md:text-lg text-white font-akkuratPro">Minutes</p>
                             </div>
                             <div className="flex flex-col justify-center gap-5 -translate-y-2 px-5">
                                 <div style={{ background: "linear-gradient(165deg, rgba(132, 93, 13, 1) 0%, rgba(162, 168, 68, 1) 56%)" }} className="w-[15px] h-[15px] rounded-full"></div>
                                 <div style={{ background: "linear-gradient(165deg, rgba(132, 93, 13, 1) 0%, rgba(162, 168, 68, 1) 56%)" }} className="w-[15px] h-[15px] rounded-full"></div>
                             </div>
                             <div className="text-center">
-                                <div style={{ background: "linear-gradient(165deg, rgba(132, 93, 13, 1) 0%, rgba(162, 168, 68, 1) 56%)" }} className="px-[20px] py-[5px] rounded-[10px] leading-[109.02px] text-[40px] md:text-[72.42px] text-[#330707] font-bold">00</div>
-                                <p className="mt-1 text-lg text-white font-akkuratPro">second</p>
+                                <div style={{ background: "linear-gradient(165deg, rgba(132, 93, 13, 1) 0%, rgba(162, 168, 68, 1) 56%)" }} className="px-[16px] md:px-[20px] py-0 md:py-[5px] rounded-[10px] leading-[109.02px] text-[30px] md:text-[72.42px] text-[#330707] font-bold">00</div>
+                                <p className="mt-1 text-[14px] md:text-lg text-white font-akkuratPro">second</p>
                             </div>
                         </div>
                     </div>
                 </div>
 
-                <div className="bg-[rgba(0,0,0,.5)] rounded-[10px] md:w-[506px] pr-2 font-akkuratPro">
-                    <div style={{ background: 'url(/images/Group2.png)', backgroundSize: 'cover', backgroundRepeat: 'no-repeat' }} className="min-h-[340px] bg-center bg-no-repeat px-[35px] pt-[50px]">
+                <div className="bg-[rgba(0,0,0,.5)] rounded-[10px] pr-2 font-akkuratPro">
+                    <div style={{ background: 'url(/images/Group2.png)', backgroundSize: 'contain', backgroundRepeat: 'no-repeat', backgroundPosition: 'center' }} className="min-h-[340px] bg-center bg-no-repeat px-[35px] pt-[60px]">
                         {/* <img src="/images/Group2.png" alt="" className="absolute top-0 left-0 w-full" /> */}
                         <div className="flex justify-between text-[#3D1105] font-bold text-[17px]">
                             <p>Price per ticket</p>
                             <p>0.0080 BNB</p>
                         </div>
 
-                        <div className="flex items-end gap-2 mt-3 justify-center">
+                        <div className="flex items-end gap-2 mt-0 justify-center">
                             <p className="ptext text-[61.02px] leading-[73.23px] -mb-2">1</p>
                             <p className="ptext2 text-[22.85px] leading-[27.42px] -mt-[10px]">Tickets</p>
                         </div>
 
-                        <div className="mt-[50px] flex flex-col gap-2">
+                        <div className="mt-[30px] flex flex-col gap-2">
                             <div className="flex justify-between text-[#3D1105] font-bold text-[19px]">
                                 <p>Price per ticket</p>
                                 <p>0.0080 BNB</p>
@@ -96,7 +96,7 @@ const Home = () => {
                         <button style={{
                             background: "linear-gradient(180deg, rgba(248, 216, 120, 1) 0%, rgba(222,139,47,1) 56%)"
                         }} className="
-                border-4 border-[#de8b2f] rounded-[8px] px-[35px] w-full py-[10px] flex justify-center items-center gap-5 text-[#3D1105] text-[19.45px] font-bold">Buy 1 tickets for 0.0080 BNB</button>
+                border-4 border-[#de8b2f] rounded-[8px] px-[35px] w-full py-[10px] flex justify-center items-center gap-5 text-[#3D1105] text-[17px] md:text-[19.45px] font-bold">Buy 1 tickets for 0.0080 BNB</button>
                     </div>
                 </div>
             </div>
@@ -111,7 +111,7 @@ const Home = () => {
                     }} className="
                 border-4 border-[#de8b2f] rounded-[8px] px-[35px] py-[10px] flex items-center gap-5 text-[#3D1105] text-[19.45px] font-bold">Telegram</button>
                 </div>
-                <div className="mx-[80px] text-white text-[19.45px] leading-[23.34px] font-[700]">Logout</div>
+                <div className="mx-[30px] md:mx-[80px] text-white text-[19.45px] leading-[23.34px] font-[700]">Logout</div>
             </div>
         </div>
     </>);
